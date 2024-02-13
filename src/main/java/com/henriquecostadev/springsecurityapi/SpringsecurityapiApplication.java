1f110ebb4276bc3f170e01608ef70d0674bd9863
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-02-13 08:59:07 -0300

    :tada: initial commit: setting up project properties!

---
+++ src/main/java/com/henriquecostadev/springsecurityapi/SpringsecurityapiApplication.java
@@ -0,0 +1,13 @@
+package com.henriquecostadev.springsecurityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquecostadev/springsecurityapi/SpringsecurityapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquecostadev.springsecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
