c5aab6a12afc4135cb4049211ef103b990423e88
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-02-13 09:13:27 -0300

    :sparkles: feat: implemented UserDetails interface on User model

--- src/main/java/com/henriquecostadev/springsecurityapi/model/User.java
+++ src/main/java/com/henriquecostadev/springsecurityapi/model/User.java
@@ -1,10 +1,16 @@
 package com.henriquecostadev.springsecurityapi.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -73,4 +79,30 @@ public class User {
     public void setRole(Role role) {
         this.role = role;
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
 }
